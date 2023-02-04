c8f06769603581dcc91970dc409162adb65740d4
Silver0x10 <smanganar@gmail.com>
2023-02-04 12:39:58 +0100

    first try to consider orientation

--- src/python/visualization.py
+++ src/python/visualization.py
@@ -6,10 +6,10 @@ import json
 import argparse
 from pathlib import Path
 
-from utils import json2dict
+from utils import json2dict, rpy2rotation_matrix
 from multi_level_surface_map import MultiLevelSurfaceMap
 
-def get_coordinates(position, size):
+def get_coordinates(position, size, orientation):
     X = [[[0, 1, 0], [0, 0, 0], [1, 0, 0], [1, 1, 0]],
          [[0, 0, 0], [0, 0, 1], [1, 0, 1], [1, 0, 0]],
          [[1, 0, 1], [1, 0, 0], [1, 1, 0], [1, 1, 1]],
@@ -20,16 +20,24 @@ def get_coordinates(position, size):
     for i in range(3):
         if i == 2: X[:,:,i] *= -size[i] # for convenience wrt the depth definition
         else: X[:,:,i] *= size[i]
+    R = rpy2rotation_matrix(orientation)
+    for i in range(X.shape[0]):
+        for j in range(X.shape[1]):
+            X[i,j,:] = R.dot(X[i,j,:])
     X += np.array(position)
     return X
 
 def get_poly_collection(scene):
-    # ToDo: separate boxes and flat surfaces
+    # TODO: separate boxes and flat surfaces
     collection = []
     for object in scene:
-        obj_position = scene[object]['position']
         obj_dimensions = scene[object]['size']
-        block = get_coordinates(obj_position, obj_dimensions)
+        obj_orientation = scene[object]['orientation']
+        obj_position = scene[object]['position']
+        obj_position[0] -= obj_dimensions[0]/2
+        obj_position[1] -= obj_dimensions[1]/2
+        obj_position[2] += obj_dimensions[2]/2
+        block = get_coordinates(obj_position, obj_dimensions, obj_orientation)
         collection.append( block )
     collection = list(np.concatenate(collection))
     return Poly3DCollection(collection, edgecolor=[0.5, 0.5, 0.5])
@@ -40,7 +48,7 @@ def main(world_json, resolution):
 
     # Multilevel map plot
     map = MultiLevelSurfaceMap(world_json, resolution)
-    map.plot()
+    #map.plot()
     
     # World plot
     world_figure = plt.figure('Wold of cubes')
@@ -54,16 +62,19 @@ def main(world_json, resolution):
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
     # Visualization boundaries:
-    ax.set_xlim([map.world_dimensions[0][0], map.world_dimensions[0][1]])
-    ax.set_ylim([map.world_dimensions[1][0], map.world_dimensions[1][1]])
-    ax.set_zlim([map.world_dimensions[2][0], map.world_dimensions[2][1]])
+    # ax.set_xlim([map.world_dimensions[0][0], map.world_dimensions[0][1]])
+    # ax.set_ylim([map.world_dimensions[1][0], map.world_dimensions[1][1]])
+    # ax.set_zlim([map.world_dimensions[2][0], map.world_dimensions[2][1]])
+    ax.set_xlim(-4, 6)
+    ax.set_ylim(-4, 6)
+    ax.set_zlim(-4, 6)
     
     plt.show()
 
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--world-json', type=Path, default='data/world.json', help='json file containing the information about the boxes contained in the world')
+    parser.add_argument('--world-json', type=Path, default='data/world_of_stairs.json', help='json file containing the information about the boxes contained in the world')
     parser.add_argument('--resolution', type=float, default=0.02, help='Set the map resolution')
     opt = parser.parse_args()
     return opt
