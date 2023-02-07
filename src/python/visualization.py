27bbf22881a6b16bc05762ca99f245dc76e70faf
Silver0x10 <smanganar@gmail.com>
2023-02-07 02:40:53 +0100

    mlsm visualization with plotly(no matplot anymore)

--- src/python/visualization.py
+++ src/python/visualization.py
@@ -6,60 +6,34 @@ import json
 import argparse
 from pathlib import Path
 from math import cos, sin
+import plotly.graph_objects as go
 
-from utils import json2dict, rpy2rotation_matrix, get_z_rotation_matrix
+# Our imports
+from utils import json2dict, rpy2rotation_matrix, get_z_rotation_matrix, get_3d_cuboid_coordinates
 from multi_level_surface_map import MultiLevelSurfaceMap
-
-def get_coordinates(position, size, orientation):
-    X = [[[0, 1, 0], [0, 0, 0], [1, 0, 0], [1, 1, 0]],
-         [[0, 0, 0], [0, 0, 1], [1, 0, 1], [1, 0, 0]],
-         [[1, 0, 1], [1, 0, 0], [1, 1, 0], [1, 1, 1]],
-         [[0, 0, 1], [0, 0, 0], [0, 1, 0], [0, 1, 1]],
-         [[0, 1, 0], [0, 1, 1], [1, 1, 1], [1, 1, 0]],
-         [[0, 1, 1], [0, 0, 1], [1, 0, 1], [1, 1, 1]]] # a list of vertices for each face of a cuboid
-    X = np.array(X).astype(float)
-
-    # Scaling based on the size
-    for i in range(3):
-        if i == 2: X[:,:,i] *= -size[i] # for convenience wrt the depth definition
-        else: X[:,:,i] *= size[i]
-
-    # Rotation based on the orientation
-    rotation_matrix = rpy2rotation_matrix(orientation)
-    for i in range(X.shape[0]):
-        for j in range(X.shape[1]):
-            X[i,j,:] = rotation_matrix.dot(X[i,j,:])
-
-    # Calculation of the position of the 'reference vertex' given the centroid of the cuboid
-    delta = np.array([-size[0]/2, -size[1]/2, size[2]/2]).astype(float)
-    delta = rotation_matrix.dot( delta )
-    position = np.array(position) + delta
-
-    # Translation based on the 'reference vertex'
-    X += position
-
-    return X
 
 
 def get_poly_collection(scene):
+    '''scene: a dictionary containing all the info about a scene. For each object it is specified the position of its centroind, its size and its orientation in a RPY-format.
+    This function will return a list of poligons that can be showed through matplotlib.'''
     # TODO: separate boxes and flat surfaces
     collection = []
     for object in scene:
         obj_size = scene[object]['size']
         obj_orientation = scene[object]['orientation']
         obj_position = scene[object]['position']
-        block = get_coordinates(obj_position, obj_size, obj_orientation)
+        block = get_3d_cuboid_coordinates(obj_position, obj_size, obj_orientation)
         collection.append( block )
     collection = list(np.concatenate(collection))
     return Poly3DCollection(collection, edgecolor=[0.5, 0.5, 0.5])
     # return Poly3DCollection(collection, shade=True) # TODO: better but works only with matplotlib v.3.7 and above
 
 
-def main(world_json, resolution):
+def old_main(world_json, resolution):
 
     # Multilevel map plot
-    map = MultiLevelSurfaceMap(world_json, resolution)
-    #map.plot()
+    # map = MultiLevelSurfaceMap(world_json, resolution)
+    # map.plot()
 
     # World plot
     world_figure = plt.figure('Wold of cubes')
@@ -83,6 +57,33 @@ def main(world_json, resolution):
     plt.show()
 
 
+def main(world_json, resolution):
+
+    # Multilevel map plot
+    map = MultiLevelSurfaceMap(world_json, resolution)
+    x, y, z = map.as_numpy(stride=1)
+    
+    # TODO add subplot of the world made by cubes
+    
+    fig = go.Figure(data=[go.Scatter3d(
+        x=x,
+        y=y,
+        z=z,
+        mode='markers',
+        marker=dict(
+            size=12,
+            color=z,                # set color to an array/list of desired values
+            colorscale='Viridis',   # choose a colorscale
+            opacity=0.8
+        )
+    )])
+
+    # tight layout
+    fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+    fig.show()
+
+
+
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--world-json', type=Path, default='data/world_of_stairs.json', help='json file containing the information about the boxes contained in the world')
@@ -93,4 +94,8 @@ def parse_opt():
 
 if __name__ == '__main__':
     opt = parse_opt()
+    # old_main(**vars(opt))
     main(**vars(opt))
+
+
+
