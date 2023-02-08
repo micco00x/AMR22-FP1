4077c0eabb63c0c82605a1ec483ae1451ed45918
Edoardo Colonna <colonna17@gmail.com>
2023-02-08 20:46:01 +0100

    Check and debug of the first part of RRT

--- src/python/planner.py
+++ src/python/planner.py
@@ -64,32 +64,36 @@ class Node():
 
 
 
-def RRT(initial_Stance, goal, map, time_max):
+def RRT(initial_Stance, goal, mlsm, time_max):
     """
     Initial_Stance = a list of 2 elements [f_swg_ini, f_sup_ini]
     Goal = a final goal region [an (x,y) area of the map]
     Map = multilevel surface map in 3D (x,y,z)
     """
     rrt_tree = Tree(initial_Stance[0], initial_Stance[1])
-    x_range, y_range, z_range = map.calculate_world_dimesions()
+    x_range = mlsm.rows
+    y_range = mlsm.columns
     #AGGIUNGERE CHECK SU initial_Stance PER VERIFICARE CHE SIA NEI LIMITI DELLA MAPPA
+    if goal_Check(rrt_tree.root, goal, mlsm) is True:
+        print('PATH FOUND')
+        return rrt_tree
     
-
-    """ Step 1) Selecting a vertex for expansion"""
+    
     for i in range(time_max):
-        if goal_Check():
-            return # TODO PATH la lista di passi da fare
-        
-        p_rand = [randint(x_range[0], x_range[1]), randint(y_range[0],y_range[1])] # random point in (x,y)
+        """ Step 1) Selecting a vertex for expansion"""
+        p_rand = [randint(0, x_range), randint(0,y_range)] # random point in (x,y)
         distance, v_near = v_near_Generation(rrt_tree.root, p_rand)
 
         """ Step 2) Generating a candidate vertex"""
         #Now let's generate  a candidate vertex. we need a set U of primitives i.e. a set of landings for the swg foot with respect to the support foot. 
         candidate_swg_f, candidate_sup_f = motion_Primitive_selector(v_near)
-        candidate_sup_f[2] = assign_height(candidate_sup_f, v_near.f_swg, map)
+        #print('candidate_sup-fot type:', type(candidate_sup_f), candidate_sup_f)
+        candidate_sup_f[2] = assign_height(candidate_sup_f, v_near.f_swg, mlsm)
+        if candidate_sup_f[2] == False: # there isn't any object or surface in this point, so discard
+            pass
         #Before creating the vertex( a node) we need first to check R1 and R2 for candidate support foot
 
-        r1_check =r1_feasibility(candidate_sup_f, map)
+        r1_check = r1_feasibility(candidate_sup_f, mlsm)
         r2_check = r2_feasibility(candidate_sup_f, v_near.f_swg)
         if r1_check == 'Not_Feasible':
             pass # The current expansion attempt is aborted and a new iteration is started
@@ -99,9 +103,10 @@ def RRT(initial_Stance, goal, map, time_max):
         """ Step 3) Choosing a parent"""
         # DEVO: DEFINIRE LA FUNZIONE NEIGHBORHOOD CHE DEFINISCE I NODI VICINI AD UN NODO,
         #       DEFINIRE LA FUNZIONE DI COSTO DI UN VERTEX
+    
 
-
-
+        # if goal_Check(f_sup, goal, mlsm):
+        #     return # TODO PATH la lista di passi da fare
 
 
         
@@ -125,18 +130,42 @@ def v_near_Generation(node, p_rand):
             best_distance = child_distance
             v_near = child_v_near
             
-    return best_distance, v_near
+    return best_distance, v_near 
 
 
 def vertex_to_Point_Distance(vertex, point, k = 3):
     
-    mid_point = [abs((vertex.f_swg[0] + vertex.f_sup[0])/2), abs((vertex.f_swg[1] + vertex.f_sup[1])/2)]
-    Saggital_axis = (vertex.f_swg[3] + vertex.f_sup[3]) / 2 # Saggital axis expressed as an angle with respect the Xo axis of the originpytoh
-    joining_vector = [abs(mid_point[0] - point[0]), abs(mid_point[1] - point[1])]
+    mid_point = np.array([abs((vertex.f_swg[0] + vertex.f_sup[0])/2), abs((vertex.f_swg[1] + vertex.f_sup[1])/2)])
+    Saggital_axis = np.array((vertex.f_swg[3] + vertex.f_sup[3]) / 2) # Saggital axis expressed as an angle with respect the Xo axis of the originpytoh
+    joining_vector =np.array([abs(mid_point[0] - point[0]), abs(mid_point[1] - point[1])])
     phi = np.arctan2(joining_vector[1], joining_vector[0])
     dist = norm((mid_point - point)) + k*abs(Saggital_axis - phi)
     
-    return dist
+    return dist # Result is in FOOT COORDS
+
+def footstep_to_footstep_metric(f1, f2, kj = 0.4):
+    p = np.array([f1[0] - f2[0], f1[1] - f2[1], f1[2] - f2[2]])
+    angle = np.array([f1[3] - f2[3]])
+    metric = norm(p) + kj*norm(angle)
+    return metric # Result is in FOOT COORDS
+
+def neighborhood(vertex,node):
+    # neighbors is a list with all the node of the tree that 
+    neighbors = []
+    if len(node.children) == 0:
+        neighbors.append(footstep_to_footstep_metric(vertex.f_sup, node.f_sup))
+        return neighbors
+    for child in node.children:
+        neighbors.append(neighborhood(vertex, child))
+    return neighbors
+
+def cost_of_a_vertex():
+    pass
+
+
+
+    
+
 
 
 def motion_Primitive_selector(node):
@@ -150,7 +179,7 @@ def motion_Primitive_selector(node):
     U_l = primitives for f_swg_id = 'Left'
     U_r = primitives for f_swg_id = 'Right
     (Recall: foot dimensions are 12x7 units)
-    It return
+    
     """
     swing = node.f_swg
     support = node.f_sup 
@@ -158,47 +187,64 @@ def motion_Primitive_selector(node):
     Saggital_axis = (swing[3] + support[3]) / 2 
     s = Saggital_axis
 
-    U_l = [[X_sup + 30, Y_sup + 12, 0, s], [X_sup + 20, Y_sup + 12, 0, s], [X_sup + 10, Y_sup + 12, 0, s], [X_sup , Y_sup + 12, 0, s], [X_sup -10, Y_sup + 12, 0, s],
-           [X_sup + 30, Y_sup + 12, 0, s + (pi/6)], [X_sup + 20, Y_sup + 12, 0, s + (pi/6)], [X_sup + 10, Y_sup + 12, 0, s + (pi/6)], [X_sup , Y_sup + 12, 0, s + (pi/6)], [X_sup -10, Y_sup - 12, 0, s + (pi/6)],
-           [X_sup + 30, Y_sup - 24, 0, s], [X_sup + 20, Y_sup - 24, 0, s], [X_sup + 10, Y_sup - 24, 0, s], [X_sup , Y_sup - 24, 0, s], [X_sup -10, Y_sup - 24, 0, s],
-           [X_sup + 30, Y_sup - 24, 0, s + (pi/6)], [X_sup + 20, Y_sup - 24, 0, s + (pi/6)], [X_sup + 10, Y_sup - 24, 0, s + (pi/6)], [X_sup , Y_sup - 24, 0, s + (pi/6)], [X_sup -10, Y_sup - 24, 0, s + (pi/6)]
+    U_l = [[X_sup + 0.30, Y_sup + 0.12, 0, s], [X_sup + 0.20, Y_sup + 0.12, 0, s], [X_sup + 0.10, Y_sup + 0.12, 0, s], [X_sup , Y_sup + 0.12, 0, s], [X_sup -0.10, Y_sup + 0.12, 0, s],
+           [X_sup + 0.30, Y_sup + 0.12, 0, s + (pi/6)], [X_sup + 0.20, Y_sup + 0.12, 0, s + (pi/6)], [X_sup + 0.10, Y_sup + 0.12, 0, s + (pi/6)], [X_sup , Y_sup + 0.12, 0, s + (pi/6)], [X_sup -0.10, Y_sup - 0.12, 0, s + (pi/6)],
+           [X_sup + 0.30, Y_sup - 0.24, 0, s], [X_sup + 0.20, Y_sup - 0.24, 0, s], [X_sup + 0.10, Y_sup - 0.24, 0, s], [X_sup , Y_sup - 0.24, 0, s], [X_sup -0.10, Y_sup - 0.24, 0, s],
+           [X_sup + 0.30, Y_sup - 0.24, 0, s + (pi/6)], [X_sup + 0.20, Y_sup - 0.24, 0, s + (pi/6)], [X_sup + 0.10, Y_sup - 0.24, 0, s + (pi/6)], [X_sup , Y_sup - 0.24, 0, s + (pi/6)], [X_sup -0.10, Y_sup - 0.24, 0, s + (pi/6)]
             ]
 
-    U_r = [[X_sup + 30, Y_sup - 12, 0, s], [X_sup + 20, Y_sup - 12, 0, s], [X_sup + 10, Y_sup - 12, 0, s], [X_sup , Y_sup - 12, 0, s], [X_sup -10, Y_sup - 12, 0, s],
-           [X_sup + 30, Y_sup - 12, 0, s + (pi/6)], [X_sup + 20, Y_sup - 12, 0, s + (pi/6)], [X_sup + 10, Y_sup - 12, 0, s + (pi/6)], [X_sup , Y_sup - 12, 0, s + (pi/6)], [X_sup -10, Y_sup - 12, 0, s + (pi/6)],
-           [X_sup + 30, Y_sup - 24, 0, s], [X_sup + 20, Y_sup - 24, 0, s], [X_sup + 10, Y_sup - 24, 0, s], [X_sup , Y_sup - 24, 0, s], [X_sup -10, Y_sup - 24, 0, s],
-           [X_sup + 30, Y_sup - 24, 0, s + (pi/6)], [X_sup + 20, Y_sup - 24, 0, s + (pi/6)], [X_sup + 10, Y_sup - 24, 0, s + (pi/6)], [X_sup , Y_sup - 24, 0, s + (pi/6)], [X_sup -10, Y_sup - 24, 0, s + (pi/6)]
+    U_r = [[X_sup + 0.30, Y_sup - 0.12, 0, s], [X_sup + 0.20, Y_sup - 0.12, 0, s], [X_sup + 0.10, Y_sup - 0.12, 0, s], [X_sup , Y_sup - 0.12, 0, s], [X_sup -0.10, Y_sup - 0.12, 0, s],
+           [X_sup + 0.30, Y_sup - 0.12, 0, s + (pi/6)], [X_sup + 0.20, Y_sup - 0.12, 0, s + (pi/6)], [X_sup + 0.10, Y_sup - 0.12, 0, s + (pi/6)], [X_sup , Y_sup - 12, 0, s + (pi/6)], [X_sup -0.10, Y_sup - 0.12, 0, s + (pi/6)],
+           [X_sup + 0.30, Y_sup - 0.24, 0, s], [X_sup + 0.20, Y_sup - 0.24, 0, s], [X_sup + 0.10, Y_sup - 0.24, 0, s], [X_sup , Y_sup - 0.24, 0, s], [X_sup -0.10, Y_sup - 0.24, 0, s],
+           [X_sup + 0.30, Y_sup - 0.24, 0, s + (pi/6)], [X_sup + 0.20, Y_sup - 0.24, 0, s + (pi/6)], [X_sup + 0.10, Y_sup - 0.24, 0, s + (pi/6)], [X_sup , Y_sup - 0.24, 0, s + (pi/6)], [X_sup -0.10, Y_sup - 0.24, 0, s + (pi/6)]
             ]
                     
 
     if node.f_swg_id == 'Left':
         new_support_foot = random.choice(U_l)
+        #print('new_support_foot:', type(new_support_foot), '  ',new_support_foot)
         pass
     if node.f_swg_id == 'Right':
         new_support_foot = random.choice(U_r )
+        #print('new_support_foot:', type(new_support_foot),'  ',new_support_foot)
         pass
 
     new_swing_foot = support
     
-    return new_swing_foot, new_support_foot
+    return new_swing_foot, new_support_foot #Result is in FOOT COORDS
 
 
-def goal_Check(info):
+def goal_Check(node, goal, mlsm):
+    f = node.f_sup
+    f_x, f_y = mlsm.world2map_coordinates(f[0], f[1])
+    for goal_point in goal:
+        if f_x == goal_point[0] and f_y == goal_point[1]: # and f[2] == goal[2]:
+            return True
+    return False # Result is in MAP COORDS
+
+
+
     
-    pass
+    
+    
 
 def assign_height(actual_footprint, previous_footprint, map):
     h_prev = previous_footprint[2]
-    tuples = map.mlsm[actual_footprint[0]][actual_footprint[1]]
-    h_actual = tuples[0][0]
-    for v in tuples: # choose the smallest height difference for actual_footprint position in the map
+    #print('h_prev :', type(h_prev))
+    cell = map.mlsm[actual_footprint[0]][actual_footprint[1]]
+    #print('cell type:', type(cell), cell)
+    if cell == None:# DA CAMBIARE EVNTUALMENTE IN multi_levele_surface_map.py , I NONE ROMPONO LE SCATOLE
+        return False
+    h_actual = cell[0][0]
+    #print('h_actual type:', type(h_actual))
+    for v in cell: # choose the smallest height difference for actual_footprint position in the map
+        print('v[0] type:', type(v[0]), v[0])
+        # print('h_prev type:' , type(h_prev))
+        # print('h_actual type', type(h_actual))
+
         if abs(v[0] - h_prev) < abs(h_actual - h_prev):
             h_actual = v[0]
-    return h_actual
-
-
-
-    pass
+    return h_actual 
 
 
 def Feasibility_check(f):
@@ -208,15 +254,16 @@ def Feasibility_check(f):
     #R3: 
     pass
 
-def r1_feasibility(f, map):
+def r1_feasibility(f, map):###DA CAMBIAREEEEE: PRIMA CALCOLO DOVE STA IL PIEDE NEL MONDO, POI CAMBIO LE COORDS E VADO NEL
+    #SISTEMA DI RIFERIMENTO DELLA MAPPA E SOLO LA VALUTO QUALE QUADRATINI OCCUPA E POI FACCIO IL CHECK
     """
     This verifies that the footstep f is fully in contact within a single horizontal patch.
     To guarantee this, each cell of the map belonging to or overlapping with the footprint
     must have the same height Z. The foorprint is 12x7 units , but let's use 13x9 to overcome
     small errors in postion
     """
-    Saggital_axis = (f[3] + f[3]) / 2
-    a = Saggital_axis
+    saggital_axis = (f[3] + f[3]) / 2
+    a = saggital_axis
     rotation_matrix = np.array(([cos(a),-sin(a)], [sin(a), cos(a)]))
     x_range = list(range(-6, +6+1))
     y_range = list(range(-4, +4+1))
@@ -227,23 +274,24 @@ def r1_feasibility(f, map):
             counter += 1
             footstep[:,counter] = np.array([j,k])
 
-    height = None
+    height = 'start'
     for i in range(0,footstep.shape[1]):
         position = np.rint(np.matmul(rotation_matrix, footstep[:,i]))
         position[0] = position[0] + f[0]
         position[1] = position[1] + f[1]
-        print(footstep[:,i], position)
-        if height == None:
+        #print(footstep[:,i], position)
+        if height == 'start':
+            if map.mlsm[position[0].astype(int)][position[1].astype(int)] == None: #SEI FUORI DALLA MAPPA CALPESTABILE , NESSUN OGGETTO
+                return 'Not_Feasible'
             height = map.mlsm[position[0].astype(int)][position[1].astype(int)][0]
         else:
+            if map.mlsm[position[0].astype(int)][position[1].astype(int)] == None: #SEI FUORI DALLA MAPPA CALPESTABILE , NESSUN OGGETTO
+                return 'Not_Feasible'
             if map.mlsm[position[0].astype(int)][position[1].astype(int)][0] == height:
                 pass
             else:
-                R1_feasibility = 'Not_Feasible'
-                return R1_feasibility 
-    R1_feasibility = 'Feasible'
-    return R1_feasibility
-
+                return 'Not_Feasible'
+    return 'Feasible'
 
 def r2_feasibility(f, f_prev):
     """
@@ -251,24 +299,26 @@ def r2_feasibility(f, f_prev):
     The constraints on the 4 dimensions of a footstep( R3 x SO(2)) are chosen by construction, based on 
     dimensions of the robot and the environment. Deltas are the maximum increments acceptable.
     """
-    delta_x_min = 1/res
-    delta_x_max = 1/res
-    delta_y_min = 1/res
-    delta_y_max = 1/res
-    delta_z_min = 1/res
-    delta_z_max = 1/res
-    delta_theta_min = 1/res
-    delta_theta_max = 1 /res
-    l = 1/res #NON MI È CHIARO CHE PARAMETRO SIA. FORSE LA DISTANZA STANDARD LUNGO L'ASSE Y TRA IL PIEDE DESTRO E SINISTRO. CHIEDERE  MICHELE
+    delta_x_min = 1
+    delta_x_max = 1
+    delta_y_min = 1
+    delta_y_max = 1
+    delta_z_min = 1
+    delta_z_max = 1
+    delta_theta_min = 1
+    delta_theta_max = 1 
+    l = 1 #NON MI È CHIARO CHE PARAMETRO SIA. FORSE LA DISTANZA STANDARD LUNGO L'ASSE Y TRA IL PIEDE DESTRO E SINISTRO. CHIEDERE  MICHELE
 
     rotation_matrix = np.array(([cos(f_prev[3]),-sin(f_prev[3])], [sin(f_prev[3]), cos(f_prev[3])]))
-    vars= np.array([f[0] - f_prev[0], [f(1) - f_prev(1)]]) # 2x1 matrix with x and y values
+    vars= np.array([[f[0] - f_prev[0]], [f[0] - f_prev[1]]]) # 2x1 matrix with x and y values
     xy_pos = np.matmul(rotation_matrix, vars) + np.array([[0], [+l]]) # positive l
+    #print('xy_pos type:',type(xy_pos), xy_pos[0], xy_pos[1])
     xy_neg = np.matmul(rotation_matrix, vars) + np.array([[0], [-l]]) #negative l
+    #print('f type', type(f[2]), '     ', 'f_prev type:', type(f_prev[2]) )
     z = f[2] - f_prev[2]
     theta = f[3] - f_prev[3]
 
-    if ((xy_pos[0] < - delta_x_min) or (xy_pos > delta_x_max)): # For x the posutive case is enough since it has 0 in the summed vector
+    if ((xy_pos[0] < - delta_x_min) or (xy_pos[0] > delta_x_max)): # For x the posutive case is enough since it has 0 in the summed vector
         return 'Not_Feasible'
     if ((xy_pos[1] < -delta_y_min) or (xy_pos[1] > delta_y_max)):
         return 'Not_Feasible'
