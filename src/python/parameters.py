f7f5bb535cfabd91c59af481aa6b8134052f4595
Silver0x10 <smanganar@gmail.com>
2023-02-19 22:14:45 +0100

    parameters container

--- src/python/parameters.py
+++ src/python/parameters.py
@@ -1,2 +1,32 @@
- 
-ROBOT_FOOT_SIZE = (0.24, 0.14)
+
+ROBOT_FOOT_SIZE = (0.24, 0.14) # (m, m)
+
+K_MU = 1
+K_GAMMA = 0.4
+
+# Original primitives: # TODO modified value. ask for what to use
+# PRIMITIVES_X = [-0.08, 0, 0.08, 0.16, 0.24] # m
+# PRIMITIVES_Y = [ 0.20, 0.30 ] # m
+# PRIMITIVES_THETA = [ 0, 0.4] # rad
+# Primitives for tests:
+PRIMITIVES_X = [-0.07, 0, 0.08, 0.16, 0.23] # m
+# PRIMITIVES_Y = [ -0.24, -0.12, 0, 0.12, 0.24] # m
+PRIMITIVES_Y = [ -0.07, 0, 0.07 ] # m
+PRIMITIVES_THETA = [ -0.2, 0, 0.2 ] # rad
+
+H_MIN = 0.02 # m
+H_MAX = 0.24 # m
+DELTA_H = 0.02 # m
+
+# For stance feasibility:
+DELTA_X_NEG = 0.08 # m
+DELTA_X_POS = 0.24 # m
+DELTA_Y_NEG = 0.25 # m # TODO modified value. ask for what to use
+DELTA_Y_POS = 0.25 # m # TODO modified value. ask for what to use
+DELTA_Z_NEG = 0.16 # m
+DELTA_Z_POS = 0.16 # m
+DELTA_THETA_NEG = 0.3 # rad
+DELTA_THETA_POS = 0.3 # rad
+L = 0.25  # m
+
+MLSM_RESOLUTION = 0.02 # m
