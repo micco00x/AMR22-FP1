59bc90a56f5b3f855db5bbbeed22a05d4aa1e45c
Silver0x10 <smanganar@gmail.com>
2023-02-19 13:28:27 +0100

    goal check/draw; p_rand 3d; cleaning&renaming

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
 #!/usr/bin/env python3
 
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import numpy as np
-import json
 import argparse
 from pathlib import Path
-from math import cos, sin
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 # Our imports
-from src.python.utils import json2dict, get_world_meshes, get_3d_cuboid_coordinates, get_2d_rectangle_coordinates
+from src.python.utils import json2dict, get_world_meshes, get_2d_rectangle_coordinates, get_2d_circle_coordinates
 from src.python.multi_level_surface_map import MultiLevelSurfaceMap
 from src.python.planner import RRT
+from src.python.parameters import *
 
 
-def main(world_json, resolution, time_max):
+def main(world_json, resolution, time_max, print_steps):
     fig = make_subplots(
-    cols=2,
-    specs=[[{"type": "scene"}, {"type": "scene"}]],
+        cols=2,
+        specs=[[{"type": "scene"}, {"type": "scene"}]],
     )
 
     # MultiLevelSurfaceMap
     map = MultiLevelSurfaceMap(world_json, resolution)
     fig.add_trace(map.as_showable(), row=1, col=1)
-    
     
     # World of stairs
     scene = json2dict(world_json)['boxes']
@@ -34,22 +29,34 @@ def main(world_json, resolution, time_max):
     for mesh in world_meshes:
         fig.add_trace(mesh, row=1, col=2)
     
-    #RRT: CAMBIO I PIEDI INIZIALI E LI METTO VICINO A DELLE SCALE PER VEDERE SE CAMBIA L' ALTEZZA IN DEI NODI SUCCESSIVI
-    # f_swg = [1.85, 2, 0.00020, np.pi/2] 
-    # f_sup = [1.65, 2, 0.00020, np.pi/2]
-    #f_swg = [-2.8, 0.9, 0.000205, np.pi/2] 
-    #f_sup = [-2.6, 0.9, 0.000205, np.pi/2]
-    f_swg = [3.3, -0.2, 0.000205, -np.pi/2] #tunnel map
-    f_sup = [3.3, 0, 0.000205, -np.pi/2]    #tunnel map
-    initial_stance = (f_swg, f_sup)
-    g = map.world2map_coordinates(1.85, 1.5)
-    f = map.world2map_coordinates(0.45, -2.3)
-    #goal_region = [ [g[0], g[1], 0.00205], [41, -31, 0.00205],[60, -141, 0.00205],[60, -141, 0.00205],[45, -70, 0.00205]]
-    goal_region = [[-3.1, -0.5, 0.00205], [-4, -0.5, 0.00205],[-3.1, 0.4, 0.00205],[-4, 0.4, 0.00205]]  #tunnel map
-    print('goal region:', goal_region, 'Start', f)
-
-    # Show goal regions
+    # Define initial stance and goal region
+    if(str(world_json) == 'data/world_of_stairs.json'):
+        f_swg = [1.85, 2, 0.00020, np.pi/2] 
+        f_sup = [1.65, 2, 0.00020, np.pi/2]
+        goal_region = (1.85, 2, 3.005, 0.5) # (x, y, z, radius)
+    elif(str(world_json) == 'data/easy.json'):
+        f_swg = [-2.2, 1.1, 0.000205, np.pi/2] 
+        f_sup = [-2.4, 1.1, 0.000205, np.pi/2]
+        goal_region = (-2.3, 10, 0.728, 0.5) # (x, y, z, radius)
+    elif(str(world_json) == 'data/tunnel.json'): # TODO correct initial configuration
+        f_swg = [3.3, -0.2, 0.000205, -np.pi/2]
+        f_sup = [3.3, 0, 0.000205, -np.pi/2]
+        goal_region = (-3.1, -0.5, 0.00205, 0.5) # (x, y, z, radius)
+        # goal_region = [[-3.1, -0.5, 0.00205], [-4, -0.5, 0.00205],[-3.1, 0.4, 0.00205],[-4, 0.4, 0.00205]]  #tunnel map
+    else:
+        print('Initial stance and goal region not specified!')
+        return
     
+    initial_stance = (f_swg, f_sup)
+    
+    # Draw the goal region:
+    goal_vertices = get_2d_circle_coordinates((goal_region[0], goal_region[1]), goal_region[3])
+    goal_x = goal_vertices[:,0]
+    goal_y = goal_vertices[:,1]
+    goal_z = np.ones(len(goal_vertices), dtype=np.float64).dot(goal_region[2]+0.01)
+    goal_mesh = go.Mesh3d(name='Goal region', x=goal_x, y=goal_y, z=goal_z, color='yellow')
+    fig.add_trace(goal_mesh, row=1, col=2)
+
     # Planning    
     steps = RRT(initial_stance, goal_region, map, time_max)
     
@@ -57,37 +64,41 @@ def main(world_json, resolution, time_max):
     for i, step in enumerate(steps):
         foot = step[0]
         foot_id = step[1]
-        print('Footprint '+ str(i) +':\t', foot, '\t', foot_id)
-        vertices = get_2d_rectangle_coordinates((foot[0],foot[1]), (0.24, 0.14), foot[3])
+        if(print_steps): print('Footprint '+ str(i) +':\t', foot, '\t', foot_id)
+        vertices = get_2d_rectangle_coordinates((foot[0],foot[1]), ROBOT_FOOT_SIZE, foot[3])
         x = vertices[:,0]
         y = vertices[:,1]
         z = np.ones(4, dtype=np.float64)
-        z = z.dot(foot[2]+0.01) # little vertical shift to avoid overlappings with objects surfaces
-        name = 'Step'+str(i-1) if i not in [0,1] else 'Start'+foot_id
-        foot_mesh = go.Mesh3d(name=name, x=x, y=y, z=z, color='cyan' if foot_id == 'Right' else 'red')
-        fig.add_trace(foot_mesh, row=1, col=2)
+        z_delta = 0.01 if i not in [0,1] else 0.1 # little vertical shift to avoid overlappings with objects surfaces and show clearly the starting position
+        z = z.dot(foot[2] + z_delta) 
+        footprint_name = 'Step'+str(i-1) if i not in [0,1] else 'Start'+foot_id
+        footprint_color = 'blue' if foot_id == 'Right' and i not in [0,1] else 'red' if i not in [0,1] else 'cyan' if foot_id == 'Right' else 'magenta'
+        footprint_mesh = go.Mesh3d(name=footprint_name, x=x, y=y, z=z, color=footprint_color)
+        fig.add_trace(footprint_mesh, row=1, col=2)
         
     fig.update_layout(margin=dict(l=0, r=0, b=0, t=0), showlegend=False)
     visualization_range = [min(map.world_dimensions[0][0], map.world_dimensions[1][0], map.world_dimensions[2][0]), max(map.world_dimensions[0][1], map.world_dimensions[1][1], map.world_dimensions[2][1])+map.resolution]
     nticks = int(visualization_range[1] - visualization_range[0])*2
     fig.update_layout(scene = dict(xaxis = dict(nticks=nticks, range=visualization_range), yaxis = dict(nticks=nticks, range=visualization_range), zaxis = dict(nticks=nticks, range=visualization_range)))
     fig.update_layout(scene2 = dict(xaxis = dict(nticks=nticks, range=visualization_range), yaxis = dict(nticks=nticks, range=visualization_range), zaxis = dict(nticks=nticks, range=visualization_range)))
+    fig.update_layout(scene_aspectmode='cube')
+    fig.update_layout(scene2_aspectmode='cube')
     fig.show()
 
 
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--world-json', type=Path, default='data/tunnel.json', help='json file containing the information about the boxes contained in the world')
+    parser.add_argument('--world-json', type=Path, default='data/world_of_stairs.json', help='json file containing the information about the boxes contained in the world')
     parser.add_argument('--resolution', type=float, default=0.02, help='Set the map resolution')
     parser.add_argument('--time-max', type=int, default=1000, help='Set the map resolution')
+    parser.add_argument('--print-steps', action='store_true', help='Print the returned steps information on the terminal')
     opt = parser.parse_args()
     return opt
 
 
 if __name__ == '__main__':
     opt = parse_opt()
-    # old_main(**vars(opt))
     main(**vars(opt))
 
 
--- src/python/planner.py
+++ src/python/planner.py
@@ -1,17 +1,11 @@
 import numpy as np
 from numpy import random
-import matplotlib.pyplot as plt
 from numpy.linalg import norm
-from numpy import transpose,  cos ,sin
 import random
-from src.python.multi_level_surface_map import MultiLevelSurfaceMap
 from src.python.utils import get_2d_rectangle_coordinates, get_z_rotation_matrix, get_z_rotation_matrix_2d
 import math
 import sympy
-from calendar import c
-from tabnanny import check
-from numpy.random import randint
-
+from tqdm import tqdm
 
 """
 Stance = (f_swing, f_support)
@@ -25,49 +19,21 @@ of the robot. In fact the initial stance of the robot is always with both feet a
 therefore both Theta_f = 0
 """
 pi = np.pi
-res = 0.5
-
-class Tree():
-    def __init__(self, f_swg_ini, f_sup_ini):
-        self.root = Node(f_swg_ini,f_sup_ini)
-    
 
 
 class Node():
     """
     Each node v of the tree is a stance v = (f_swg, f_sup)
     """
-    def __init__(self, f_swg, f_sup, cost = 0, parent = None, trajectory = []):
+    def __init__(self, f_swg, f_sup, f_swg_id = None, cost = 0, parent = None, trajectory = []):
         self.f_swg = f_swg
         self.f_sup = f_sup
         self.parent = parent
         self.children = []
         self.cost = cost
-        self.f_swg_id = 'Right' # It always starts by moving right foot first
+        self.f_swg_id = f_swg_id # It always starts by moving right foot first
         self.trajectory = trajectory # Trajectory is the trajectory that , starting from the parent, brings to the actual node
     
-    # def expand(self, new_f_swg, new_f_sup):
-    #     new_node = Node(new_f_swg, new_f_sup)
-    #     new_node.parent = self
-    #     if self.f_swg_id == 'Right':
-    #         new_node.f_swg_id = 'Left'
-    #     if self.f_swg_id == 'Left':
-    #         new_node.f_swg_id ='Right'
-
-    #     self.children = [self.children, new_node]
-
-    # def is_Child(self, parent_node):
-    #     parent_node.children.append(self)
-    #     if parent_node.f_swg_id == 'Right':
-    #         self.f_swg_id = 'Left'
-    #     if parent_node.f_swg_id == 'Left':
-    #         self.f_swg_id = 'Right'
-
-    
-    # def is_parent(self, child_node):
-    #     child_node.parent = self
-
-
     def __eq__(self, other):
         if (self.f_swg == other.f_swg) and (self.f_sup == other.f_sup):
             return True
@@ -78,105 +44,90 @@ class Node():
 
 
 
-
-
-def RRT(initial_Stance, goal, map, time_max):
+def RRT(initial_Stance, goal_region, map, time_max):
     """
     Initial_Stance = a list of 2 elements [f_swg_ini, f_sup_ini]
     Goal = a final goal region [an (x,y) area of the map]
     Map = multilevel surface map in 3D (x,y,z)
     """
-    rrt_tree = Node(initial_Stance[0], initial_Stance[1])
-    print('QUALI SONO I PIEDI', rrt_tree.f_swg, rrt_tree.f_sup)
+    rrt_root = Node(initial_Stance[0], initial_Stance[1], f_swg_id='Right')
     x_range, y_range, z_range = map.world_dimensions
-    lista_nodi = [rrt_tree]
-    # #AGGIUNGERE CHECK SU initial_Stance PER VERIFICARE CHE SIA NEI LIMITI DELLA MAPPA
-    if goal_Check(rrt_tree, goal, map) is True:
-        print('PATH FOUND')
-        return rrt_tree
     
-
+    if goal_check(rrt_root, goal_region):
+        print('Already in the goal region! No steps needed')
+        return retrieve_all_steps(rrt_root)
+    
+    goal_nodes = []
     v_candidate = None
-    for i in range(time_max):
-        print('Iteration: ', i)
+    for i in tqdm(range(time_max)):
         """ Step 1) Selecting a vertex for expansion"""
-        p_rand = [random.random()*abs(x_range[1]-x_range[0]) + x_range[0], random.random()*abs(y_range[1]-y_range[0]) + y_range[0]] # random point in (x,y)
-        cell_rand = map.query(p_rand[0], p_rand[1])
-        if cell_rand == None:
-            continue
-        #p_rand[2] = random.choice(cell_rand)[0]
-        distance, v_near = v_near_Generation(rrt_tree, p_rand) # TODO Fix this part. It modifies the root
-        #v_near = rrt_tree.root if i==0 or not v_candidate else v_candidate
+        x_rand = random.random()*abs(x_range[1] - x_range[0]) + x_range[0]
+        y_rand = random.random()*abs(y_range[1] - y_range[0]) + y_range[0]
+        z_rand = random.random()*abs(z_range[1] - z_range[0] + 3) + z_range[0] # 1.5m added on top and at the bottom to have a better distribution
+        p_rand = [x_rand, y_rand, z_rand] # random point in (x,y,z)
+        
+        distance, v_near = v_near_selection(rrt_root, p_rand) 
+        # v_near = rrt_root if i==0 or not v_candidate else v_candidate
         #print('BEST_DISTANCE:',distance)
         
                
         """ Step 2) Generating a candidate vertex"""
-        #Now let's generate  a candidate vertex. we need a set U of primitives i.e. a set of landings for the swg foot with respect to the support foot. 
-        candidate_swg_f, candidate_sup_f, candidate_id = motion_Primitive_selector(v_near) #candidate_id è il piede di swing del nodo candiate
+        # Now let's generate  a candidate vertex. we need a set U of primitives i.e. a set of landings for the swg foot with respect to the support foot. 
+        candidate_swg_f, candidate_sup_f, candidate_id = motion_primitive_selector(v_near) #candidate_id è il piede di swing del nodo candiate
         candidate_sup_f[2] = assign_height( v_near.f_swg, candidate_sup_f, map)
         if candidate_sup_f[2] == None: continue
-        #Before creating the vertex( a node) we need first to check R1 and R2 for candidate support foot
+        # Before creating the vertex( a node) we need first to check R1 and R2 for candidate support foot
 
-        # TODO Check this part
-        r1_check = r1_feasibility(candidate_sup_f, map)
-        if r1_check == False:
-            print('r1_check fail')
+        # Step feasibility checks:
+        if not r1_feasibility(candidate_sup_f, map):
+            # print('r1_check fail')
             continue # The current expansion attempt is aborted and a new iteration is started
-        r2_check = r2_feasibility(v_near.f_sup, candidate_sup_f, candidate_id)
-        if r2_check == False:
-            print('r2:check fail')
+        if not r2_feasibility(v_near.f_sup, candidate_sup_f, candidate_id):
+            # print('r2:check fail')
             continue
-        r3_check = r3_feasibility(v_near.f_swg, candidate_sup_f, map)
-        if r3_check == False:
-            print('r3:check fail')
+        if not r3_feasibility(v_near.f_swg, candidate_sup_f, map):
+            # print('r3:check fail')
             continue
         
-        v_candidate = Node(candidate_swg_f, candidate_sup_f)
-        v_candidate.parent = v_near
-        
+        v_candidate = Node(candidate_swg_f, candidate_sup_f, f_swg_id=candidate_id)
 
 
-        """ Step 3) Choosing a parent"""
-        #TODO Check this part
-        # print('VABENE')
-        neighbors = neighborhood(v_candidate, rrt_tree)
-        candidate_parent = v_near
-        candidate_cost = cost_of_a_new_vertex(v_candidate, candidate_parent) ### PER ORA IL COSTO PER PASSARE DA UN NODO AD UN ALTRO È 1, VA CAMBIATO, COSÌ È NAIVE
-        for neigh in neighbors:
-            if r2_feasibility( neigh.f_sup, candidate_sup_f, neigh.f_swg_id): ###HERE WE MUST ADD ALSO R3 FEASIBILITY
-                if r3_feasibility(neigh.f_swg, candidate_sup_f, map ):
-                    if (cost_of_a_new_vertex(v_candidate, neigh))  < candidate_cost:
-                        candidate_parent = neigh
-                        candidate_cost = cost_of_a_new_vertex(v_candidate, neigh)
+        """ Step 3) Choosing a parent""" # TODO re-check before integration
+        # neighbors = neighborhood(v_candidate, rrt_root)
+        # candidate_parent = v_near
+        # candidate_cost = cost_of_a_new_vertex(v_candidate, candidate_parent) ### PER ORA IL COSTO PER PASSARE DA UN NODO AD UN ALTRO È 1, VA CAMBIATO, COSÌ È NAIVE
+        # for neigh in neighbors:
+        #     if r2_feasibility( neigh.f_sup, candidate_sup_f, neigh.f_swg_id): ###HERE WE MUST ADD ALSO R3 FEASIBILITY
+        #         if r3_feasibility(neigh.f_swg, candidate_sup_f, map ):
+        #             if (cost_of_a_new_vertex(v_candidate, neigh)) < candidate_cost:
+        #                 candidate_parent = neigh
+        #                 candidate_cost = cost_of_a_new_vertex(v_candidate, neigh)
         
-        #now let's add the node to the tree, POI QUESTO PASSAGGIO VA FATTO DOPO IL PUNTO 4, ORA È UN TEST
-        # if not v_candidate.parent.parent:
+
+        '''Step 4): Rewiring '''
+        # rewiring(v_candidate, rrt_root, map) # TODO update the rewiring step. I think that right now it does nothing. It has to return the updated root node
+
+        # Now let's add the node to the tree
+        # # if not v_candidate.parent.parent:
         if v_candidate not in v_near.children:
-            lista_nodi.append(v_candidate)
             v_candidate.parent = v_near
             v_candidate.cost = v_near.cost + 1 #candidate_cost
             v_candidate.f_swg_id = candidate_id
             v_near.children.append(v_candidate)
-        # print( 'NEW FOOTSTEP',map.world2map_coordinates(v_candidate.f_sup[0], v_candidate.f_sup[1]), v_candidate.f_sup[2])
-        # print('FOOTSTEP ID:', v_candidate.f_swg_id )
 
-
-        '''Step 4): Rewiring '''
-        rewiring(v_candidate, rrt_tree, map)
-
-
-        if goal_Check(v_candidate, goal, map):
-            print('PATH FOUND')
+        if goal_check(v_candidate, goal_region):
+            goal_nodes.append(v_candidate)
             break
                       
+    print('\n### End RRT search ###')
 
-    print('ok')
-        # if goal_Check(f_sup, goal, mlsm):
-        #     return # TODO PATH la lista di passi da fare fino al goal
-
-    nodo_goal = lista_nodi[-1]
-    return retrieve_all_steps(rrt_tree) # Uncomment thi line to visualize all the nodes of the tree
-    # return retrieve_steps(nodo_goal)
+    try:
+        best_goal_node = min(goal_nodes, key=lambda goal_node: goal_node.cost)
+    except: # Goal_nodes is an empty list
+        print('Path to the goal NOT FOUND!\n')
+        return retrieve_all_steps(rrt_root)
+    print('Path to the goal FOUND!\n')
+    return retrieve_steps(best_goal_node)
 
 
 def retrieve_steps(node):
@@ -202,29 +153,22 @@ def retrieve_all_steps(node):
     return steps
 
 
-def goal_Check(node, goal, map):
-    f = node.f_sup
-    f_x, f_y = map.world2map_coordinates(f[0], f[1])
-    for goal_point in goal:
-        if f_x == goal_point[0] and f_y == goal_point[1]: # and f[2] == goal[2]:
-            return True
+def goal_check(node, goal):
+    if abs(goal[2] - node.f_sup[2]) < 0.01:
+        distance_to_goal = goal[3] - math.sqrt( (goal[0] - node.f_sup[0])**2 + (goal[1] - node.f_sup[1])**2 )
+        if distance_to_goal >= 0: return True
     return False # Result is in MAP COORDS            
 
 
-
-
-
-
-
-def v_near_Generation(node, p_rand):
-    #print('NODO',node)
-    best_distance = vertex_to_Point_Distance(node, p_rand)
+def v_near_selection(node, p_rand):
+    best_distance = node_to_point_distance(node, p_rand)
     v_near = node
     if len(node.children) == 0:
         return best_distance, v_near
     
+    # TODO make it iterative
     for child in node.children:
-        child_distance, child_v_near = v_near_Generation(child, p_rand)
+        child_distance, child_v_near = v_near_selection(child, p_rand)
         if child_distance < best_distance:
             best_distance = child_distance
             v_near = child_v_near
@@ -233,36 +177,25 @@ def v_near_Generation(node, p_rand):
 
 
 
-def vertex_to_Point_Distance(vertex, point, k = 3):
-    
-    # mid_point = np.array([(vertex.f_swg[0] + vertex.f_sup[0])/2, (vertex.f_swg[1] + vertex.f_sup[1])/2])
-    # Saggital_axis = np.array((vertex.f_swg[3] + vertex.f_sup[3]) / 2) # Saggital axis expressed as an angle with respect the Xo axis of the originpytoh
-    # joining_vector =np.array([abs(mid_point[0] - point[0]), abs(mid_point[1] - point[1])])
-    # phi = np.arctan2(joining_vector[1], joining_vector[0])
-    # dist = norm((mid_point - point)) + k*abs(Saggital_axis - phi)
-    
-    # return dist # Result is in FOOT COORDS
-    
-    mid_point = np.array([(vertex.f_swg[0] + vertex.f_sup[0])/2, (vertex.f_swg[1] + vertex.f_sup[1])/2])
-    Saggital_axis = np.array((vertex.f_swg[3] + vertex.f_sup[3]) / 2)
-    #print('Saggital Axis:',Saggital_axis) # Saggital axis expressed as an angle with respect the Xo axis of the originpytoh
-    joining_vector =np.array([(point[0] - mid_point[0]), (point[1] - mid_point[1])])
-    #h_mid = (vertex.f_swg[2] + vertex.f_sup[2])/2
-    #point_xy = [point[0],point[1]]
+def node_to_point_distance(node, point, k_mu = 1):
+    mid_point = np.array([(node.f_swg[0] + node.f_sup[0])/2, (node.f_swg[1] + node.f_sup[1])/2, (node.f_swg[2] + node.f_sup[2])/2])
+    saggital_axis = np.array((node.f_swg[3] + node.f_sup[3]) / 2)
+    joining_vector =np.array([(point[0] - mid_point[0]), (point[1] - mid_point[1]), (point[2] - mid_point[2])])
     phi = np.arctan2(joining_vector[1], joining_vector[0])
-    #print('Phi:', phi)
-    dist = norm((mid_point - point)) + k*abs(Saggital_axis - phi)
+    distance = norm((mid_point - point)) + k_mu*abs(saggital_axis - phi)
     
-    return dist # Result is in FOOT COORDS
+    return distance # Result is in FOOT COORDS
 
-def footstep_to_footstep_metric(f1, f2, kj = 0.4):
+
+def footstep_to_footstep_distance_metric(f1, f2, k_gamma = 0.4):
     p = np.array([f1[0] - f2[0], f1[1] - f2[1], f1[2] - f2[2]])
     angle = np.array([f1[3] - f2[3]])
-    metric = norm(p) + kj*norm(angle)
+    metric = norm(p) + k_gamma*norm(angle)
     return metric # Result is in FOOT COORDS
 
-def neighborhood(vertex, tree, r_neigh = 2):
-    # neighbors is a list with all the node of the tree that have a footstep_to_footstep metric < r_neigh w.r.t VERTEX
+
+def neighborhood(vertex, tree_root, r_neigh = 2): # TODO check this part: maybe it considers all the leaf nodes as neighbour
+    # Returns a list containing all the nodes of the tree in that have a footstep_to_footstep metric < r_neigh w.r.t VERTEX
     neighbors = []
     if len(tree.children) == 0:
         metric = footstep_to_footstep_metric(vertex.f_sup, tree.f_sup)
@@ -275,6 +208,7 @@ def neighborhood(vertex, tree, r_neigh = 2):
             neighbors.append(neigh)
     return neighbors
 
+
 def cost_of_a_new_vertex(vertex, candidate_parent):
     """ Returns an integer >= 0 as a cost to reach the vertex from the root"""
     cost = candidate_parent.cost + 1 # Da inserire metrica tra vertex e parent AL POSTO DI 1
@@ -286,7 +220,7 @@ def cost_of_a_new_vertex(vertex, candidate_parent):
 
 
 
-def motion_Primitive_selector(node):
+def motion_primitive_selector(node):
     """
     Set of primitives U: 20 possible swing foot landing w.r.t the actual support foot
     For now lets assume 5 possible forward distances, 2 side distances and 2 possible angles
@@ -317,12 +251,6 @@ def motion_Primitive_selector(node):
     delta[:-1] = rot.dot(delta[:-1])
     new_support_foot = [ x_swg + delta[0], y_swg + delta[1], z_swg, theta_swg + delta[3]]
     
-    ##ROUND DA RIVEDERE
-    '''
-    for i in range(0, 4):
-        new_support_foot[i] = round(new_support_foot[i], 5)
-    '''
-
     new_swing_foot = node.f_sup
     
     return new_swing_foot, new_support_foot, new_id
@@ -369,11 +297,10 @@ def r1_feasibility(f, map):###DA CAMBIAREEEEE: PRIMA CALCOLO DOVE STA IL PIEDE N
         cell = map.query(ver[0], ver[1])
         if cell == None:
             return False
-        if  any((obj[0] >= (f[2]) and obj[0] <= (f[2]) ) for obj in cell):
+        elif  any((obj[0] >= (f[2]) and obj[0] <= (f[2]) ) for obj in cell):
             continue
         else:
             return False
-    #print('Ce la fa')
     return  True
 
 
@@ -383,14 +310,14 @@ def r2_feasibility( f_prev, f_actual, candiate_swg_id): #f_prev è il piede di s
     The constraints on the 4 dimensions of a footstep( R3 x SO(2)) are chosen by construction, based on 
     dimensions of the robot and the environment. Deltas are the maximum increments acceptable.
     """
-    delta_x_neg = 0.30
-    delta_x_pos = 0.40
-    delta_y_neg = 0.5
-    delta_y_pos = 0.5
-    delta_z_neg = 0.25
-    delta_z_pos = 0.25
-    delta_theta_neg = pi/4
-    delta_theta_pos =pi/4
+    delta_x_neg = 0.08
+    delta_x_pos = 0.24
+    delta_y_neg = 0.20
+    delta_y_pos = 0.20
+    delta_z_neg = 0.16
+    delta_z_pos = 0.16
+    delta_theta_neg = 0.4
+    delta_theta_pos =0.4
 
     l = 0.25 #NON MI È CHIARO CHE PARAMETRO SIA. FORSE LA DISTANZA STANDARD LUNGO L'ASSE Y TRA IL PIEDE DESTRO E SINISTRO. CHIEDERE  MICHELE
 
@@ -416,16 +343,16 @@ def r2_feasibility( f_prev, f_actual, candiate_swg_id): #f_prev è il piede di s
 
 
     if ((xy[0] < -delta_x_neg) or (xy[0] > delta_x_pos)):
-        print('X fail, difference is',xy[0])
+        # print('X fail, difference is',xy[0])
         return False
     if ((xy[1] < -delta_y_neg) or (xy[1] > delta_y_pos)):
-        print("Y_ERROR pos: ", xy[1])
+        # print("Y_ERROR pos: ", xy[1])
         return False
     if ((z < -delta_z_neg) or (z > delta_z_pos)):
-        print("z_ERROR: ", z)
+        # print("z_ERROR: ", z)
         return False
     if ((theta < -delta_theta_neg) or (theta > delta_theta_pos )):
-        print("THETA_ERROR: ", theta)
+        # print("THETA_ERROR: ", theta)
         return False
     return True
 
